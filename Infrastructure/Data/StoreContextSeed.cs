f5afd224141a3d184cb0424d6be5927992f64d6c
Andi <andy.u789@gmail.com>
2021-07-13 16:55:00 +0300

    end of section 16

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -44,6 +45,17 @@ namespace Infrastructure.Data
                     foreach (var product in products)
                     {
                         context.Products.Add(product);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.DeliveryMethods.Any())
+                {
+                    var dmData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                    var deliveryMethods = JsonSerializer.Deserialize<IList<DeliveryMethod>>(dmData);
+                    foreach (var deliveryMethod in deliveryMethods)
+                    {
+                        context.DeliveryMethods.Add(deliveryMethod);
                     }
                     await context.SaveChangesAsync();
                 }
